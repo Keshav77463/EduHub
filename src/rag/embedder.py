073fcbcca16ebd834f2c57f97db558a0e074c820
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-03-21 23:00:38 +0530

    added vector store to store embeddings

--- src/rag/embedder.py
+++ src/rag/embedder.py
@@ -5,3 +5,11 @@ def create_vector_store(chunks,persist_directory="data/chroma_db"):
     embeddings = HuggingFaceEmbeddings(
         model_name = "all-MiniLM-L6-v2"
     )
+    vector_store = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=persist_directory,
+    )
+    print("Vector store created")
+    return vector_store
+
