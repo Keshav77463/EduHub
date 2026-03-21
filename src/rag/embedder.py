f731fa63eb56aa40c5842f794d440f67fe6fc590
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-03-22 01:29:33 +0530

    updated the path

--- src/rag/embedder.py
+++ src/rag/embedder.py
@@ -1,7 +1,7 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
-def create_vector_store(chunks,persist_directory="data/chroma_db"):
+def create_vector_store(chunks,persist_directory="D:\\EduHub\\data\\chroma_db"):
     embeddings = HuggingFaceEmbeddings(
         model_name = "all-MiniLM-L6-v2"
     )
