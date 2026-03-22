7660b89def5886682a9b1351c327dc2189d096cd
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-03-22 16:23:33 +0530

    increased the value of number of retained docs

--- src/rag/retriever.py
+++ src/rag/retriever.py
@@ -1,7 +1,6 @@
-from embedder import create_vector_store
 def retrieve_documents(question,vector_store):
     result=vector_store.similarity_search(
         question,
-        k=3
+        k=5
     )
     return result
